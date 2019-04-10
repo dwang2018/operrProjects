8e65308c42b62b6361e8cc78f172a4a526a6d666
dwang2018 <40223010+dwang2018@users.noreply.github.com>
2019-04-10 17:41:38 -0400

    full stack developer
    using react.

---
+++ my-app/build/precache-manifest.d532bf2b59d11b775b456a097018a3a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "34e8e8d8c38a3e957ceb",
+    "url": "/static/js/main.7da41c29.chunk.js"
+  },
+  {
+    "revision": "5014e1f06b9a4a0beea9",
+    "url": "/static/js/2.72de90ee.chunk.js"
+  },
+  {
+    "revision": "34e8e8d8c38a3e957ceb",
+    "url": "/static/css/main.bc1bc2cc.chunk.css"
+  },
+  {
+    "revision": "5d8e9239f7c558251d751a1ffe35e062",
+    "url": "/index.html"
+  }
+];
